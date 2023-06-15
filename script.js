c1feaf04cb97b2451386b65ef58478ae0a56c9fb
Aidan Spencer <aidanspencer97@gmail.com>
2023-06-14 22:52:53 -0400

    removing old files

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var cursors = ["url(cursors/dad.cur), auto;"];                
-    $('.demo').each(function() {
-        var rand = Math.floor(Math.random()*cursors.length);
-        $(this).css("cursor", cursors[rand]);
-    });
-});
